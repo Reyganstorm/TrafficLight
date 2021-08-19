4985b9e8e51bc7832d7c2d486d7db40c25c04337
Ruslan Shtybaev <ruslan.shtybaev@gmail.com>
2021-08-19 04:03:27 +0300

    Trying to take a code  (Hardcode)

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var managerButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightView.layer.cornerRadius = 50
+        yellowLightView.layer.cornerRadius = 50
+        greenLightView.layer.cornerRadius = 50
+        managerButton.layer.cornerRadius = 10
+        
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
+        
+        
     }
 
-
+    @IBAction func managerButtonPressed() {
+        managerButton.setTitle("Next", for: .normal)
+        
+        if redLightView.alpha != 1 && yellowLightView.alpha != 1 {
+            
+            redLightView.alpha = 1
+            greenLightView.alpha = 0.3
+            
+        } else if redLightView.alpha == 1 {
+            
+            yellowLightView.alpha = 1
+            redLightView.alpha = 0.3
+            
+        } else if yellowLightView.alpha == 1 {
+            
+            greenLightView.alpha = 1
+            yellowLightView.alpha = 0.3
+        }
+    }
+    
 }
 
